acf71ff0187aeae15e9d98696a28c802a814e8ff
Mauro Lopez <lopezjimenezmauro05@gmail.com>
2023-07-11 14:39:22 +0200

    fix: uses cbor lib to set format

--- src/send.ts
+++ src/send.ts
@@ -1,3 +1,4 @@
+import cbor from 'cbor'
 import coap from 'coap'
 import { type CoapMethod } from 'coap-packet'
 
@@ -9,22 +10,23 @@ import { type CoapMethod } from 'coap-packet'
  */
 export const send = async (newManufacturer: string, cbor: boolean = false) => {
 	console.log('\nSend operation from Information Reporting interface: start')
-	console.log(`\nFormat: ${cbor === true ? 'cbor': 'json'}`)
-	const jsonValue = JSON.stringify([{ n: '/3/0/0', ['vs']: newManufacturer }])
-	const cborValue = `[{-2:"/3/0/",0:"0",3:"${newManufacturer}"}]`
-	const payload = cbor === true ? cborValue : jsonValue
+	console.log(`\nFormat: ${cbor === true ? 'cbor' : 'json'}`)
+	const jsonValue = Buffer.from(
+		JSON.stringify([{ 'n': '/3/0/0', 'vs': newManufacturer }]),
+	)
+	const payload = cbor === true ? getCborValue(newManufacturer) : jsonValue
 	const params = {
 		host: 'eu.iot.avsystem.cloud',
 		port: 5683,
 		pathname: '/dp',
 		method: 'POST' as CoapMethod,
 		options: {
-			'Content-Format': cbor === true ? 'application/senml+cbor' :'application/senml+json',
+			'Content-Format':
+				cbor === true ? 'application/senml+cbor' : 'application/senml+json',
 		},
 	}
 	const agent = new coap.Agent({ type: 'udp4' })
-	console.log(Buffer.from(payload))
-	const request = agent.request(params).end(Buffer.from(payload))
+	const request = agent.request(params).end(payload)
 
 	const serverResponse = new Promise<coap.IncomingMessage>(
 		(resolve, reject) => {
@@ -34,6 +36,7 @@ export const send = async (newManufacturer: string, cbor: boolean = false) => {
 				if (response.code === '2.01' || response.code === '2.05') {
 					return resolve(response)
 				}
+				console.log(response)
 				return reject(new Error('Server does not accept the request'))
 			})
 		},
@@ -43,4 +46,13 @@ export const send = async (newManufacturer: string, cbor: boolean = false) => {
 	await serverResponse
 		.then((value) => console.log('\nValue: ', value))
 		.catch((err) => console.log('\nError: ', err))
+}
+
+/**
+ * Set senml+cbor format
+ */
+const getCborValue = (newManufacturer: string): Buffer => {
+	const map = new Map().set(0, '/3/0/0').set(3, newManufacturer)
+	const cborValue = cbor.encode(map)
+	return cborValue
 }
